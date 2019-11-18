e3b1eb01996b67a47b74b27a0219035d94dcae68
SlickRick518 <43962417+SlickRick518@users.noreply.github.com>
2019-11-17 21:53:43 -0500

    Add files via upload

---
+++ DatabaseScripts/FreshStart.sql
@@ -0,0 +1,40 @@
+insert into doctor(DoctorName, email, password_) 
+values ('Phil Man', 'doctor@cancellation.org', 'doctor');
+
+##add json test
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you write with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you draw with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you throw with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you use scissors with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you brush your teeth with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you hold a knife with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you hold a spoon with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you hold a broom handle with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you use to strike a match?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which hand do you open a lid with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which foot do you prefer to kick with?" );  
+
+insert into questions (QuestionType, PossibleAnswers, Question) 
+values (2, "Always left#Usually left#Either hand#Usually right#Always right","Which eye do you use when using only one eye?" );  
---
+++ DatabaseScripts/VewTables.sql
@@ -0,0 +1,9 @@
+  use test;
+  use datadump;
+select name from json_files;
+select * from Doctor;  
+select * from testframe;
+select * from circles;
+select * from pressure;
+select * from answers;
+select * from json_files;
