9870ae0f7f728fba67aa2a34c4dd56ede26622dd
KartoffelMann <auriemmat6@students.rowan.edu>
2019-12-08 16:16:14 -0500

    Fixes to the excel endpoints
    Added more COLORS! Also fixed the model and code in Data/routes.py to reflect the circles model being fixed.

--- FlaskAPP/models/circles.py
+++ FlaskAPP/models/circles.py
@@ -3,7 +3,7 @@ from FlaskAPP import db
 class Circles(db.Model):
     TestID = db.Column(db.Integer, db.ForeignKey('testframe.TestID'),
         primary_key=True)
-    CircleID = db.Column(db.Integer)
+    CircleID = db.Column(db.Integer, primary_key=True)
     symbol = db.Column(db.String(1))
     begin_circle = db.Column(db.Float)
     end_circle = db.Column(db.Float)
